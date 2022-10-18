a360268d5fa7d214e52212034c67a32ec755d8d9
Dipanshu <dipanshukubde5678@gmail.com>
2022-10-18 19:27:11 +0530

    first commit

---
+++ airbnb/components/Header.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div>
+      <h1>I am Ravan</h1>
+
+      {/* // Left */}
+      <div></div>
+
+      {/* // Middle */}
+      <div></div>
+
+      {/* // Right */}
+      <div></div>
+    </div>
+  );
+}
+
+export default Header;
